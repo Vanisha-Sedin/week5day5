108fab1b3beb24439cd3bf6319dd2adbedcae461
Vanisha-Sedin <vanisha@sedintechnologies.com>
2026-01-22 00:22:21 +0530

    Update App4.jsx

--- App4.jsx
+++ App4.jsx
@@ -1,23 +1,23 @@
 import { useEffect, useState } from "react";
+import "./App4.css"; // Import the CSS file for styling
 
-// useState - Holds the list of posts (magic box)
-// useEffect - Runs after the page appears to fetch posts from the API
-
+// App4 Component - Displays a list of posts from the API
 function App4() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]); // Holds the list of posts
 
+  // Fetch posts when the component mounts
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(res => res.json())
-      .then(data => setPosts(data));
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
   }, []);
 
   return (
     <div>
       <h1>Posts</h1>
 
-      {posts.map(post => (
-        <div key={post.id}>
+      {posts.map((post) => (
+        <div className="post" key={post.id}>
           <h3>{post.title}</h3>
           <p>{post.body}</p>
           <hr />
